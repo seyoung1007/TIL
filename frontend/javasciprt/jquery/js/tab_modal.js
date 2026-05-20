c3c29476076c12852b6f31307df96711033fc151
seyoung1007 <bobb7771@gmail.com>
2026-05-20 15:52:59 +0900

    탭 기능 모달창 열기 구현

---
+++ frontend/javasciprt/jquery/js/tab_modal.js
@@ -0,0 +1,18 @@
+$(function () {
+  // 탭메뉴
+  $(".tabmenu > li > a").click(function () {
+    console.log(this);
+    $(this).parent().addClass("active").siblings().removeClass("active");
+  });
+  //   modal open
+  $(".notice li:first-child").click(function () {
+    // $(".modal").addClass("active");
+    $(".modal").show()
+  });
+  //   modal close
+  $(".modal-btn").click(function () {
+    //  $(".modal").removeClass("active");
+    $(".modal").hide()
+  });
+});
+``
